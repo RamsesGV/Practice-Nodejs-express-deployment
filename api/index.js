148057db8cc2d0c9f813f4c8577917ecbba94b33
Ramses Garay <senkuStone15@gmail.com>
2023-05-17 12:45:30 -0600

    practice deployment-Nodejs-Express

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const routerApi = require('./routes')
+//const { logErrors, errorHandler} = require('./middlewares/errorHandler')
+const server = express()
+const PORT = 3000;
+
+
+server.use(express.json())
+//server.use(logErrors())
+//server.use(errorHandler())
+
+
+
+server.listen(PORT, () => {
+console.log(`Server raised on http://localhost:${PORT}`);
+})
+routerApi(server)
